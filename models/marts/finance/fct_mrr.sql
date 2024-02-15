5f41c33e1ac3f63094d183067f3479526ff125f3
Iran Rodrigues <imrodrig@ucsc.edu>
2024-02-15 05:18:53 +0000

    mods for mock inputs for unit testing

--- models/marts/finance/fct_mrr.sql
+++ models/marts/finance/fct_mrr.sql
@@ -20,7 +20,9 @@ monthly_subscriptions AS (
         --DATE(DATE_TRUNC('month', starts_at)) AS start_month,
         --DATE(DATE_TRUNC('month', ends_at)) AS end_month
     FROM
-        {{ ref('dim_subscriptions') }}
+        --{{ ref('dim_subscriptions') }}
+        {{ unit_testing_select_table(ref('dim_subscriptions'), ref('unit_test_input_dim_subscriptions')) }}
+
     WHERE
         billing_period = 'monthly'
 ),
@@ -30,7 +32,8 @@ months AS (
     SELECT
         calendar_date AS date_month
     FROM
-        {{ ref('int_dates') }}
+        --{{ ref('int_dates') }}
+        {{ unit_testing_select_table(ref('int_dates'), ref('unit_test_input_int_dates')) }}
     WHERE
         day_of_month = 1
 ),
